22d2e970105255b102b4b5f51088ca7d3c1358af
scarlson <106626766+scarls95@users.noreply.github.com>
2022-12-06 15:44:18 -0700

    fix44

--- main.tf
+++ main.tf
@@ -35,7 +35,6 @@ module "blog_vpc" {
 module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.3"
-  # insert the 1 required variable here
 
   name = "blog"
   min_size = 1
