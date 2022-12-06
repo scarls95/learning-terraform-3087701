9b48ad342d7fbd57b30616241368dbf144ad2d2f
scarlson <106626766+scarls95@users.noreply.github.com>
2022-12-06 12:43:01 -0700

    vpc fix2

--- main.tf
+++ main.tf
@@ -20,8 +20,8 @@ module "vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  azs             = ["us-west-2a", "us-west-1b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
  
   enable_nat_gateway = true
 
@@ -29,7 +29,7 @@ module "vpc" {
     Terraform = "true"
     Environment = "dev"
   }
-  
+
 }
 
 resource "aws_instance" "blog" {
